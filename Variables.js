5bad370957a0c1573a79f752edb2e1088f292dcc
Kevin <kevinquesada707@gmail.com>
2024-11-27 12:38:34 -0500

    The name of the Js file was changed to a more descriptive one

---
+++ Variables.js
@@ -0,0 +1,37 @@
+// Definición de la variable que almacena el nombre de un juguete
+let cajaDeJuguetes = "Oso";
+console.log(cajaDeJuguetes);
+
+// Ejemplos de nombres de variables no recomendados
+
+// 'c' es demasiado genérico y no proporciona información útil sobre su contenido
+let c = "oso"; 
+
+// 'cda' no es descriptivo; no indica claramente qué representa la variable
+let cda = "oso"; 
+
+// 'pcAndy' puede llevar a confusión; 'pc' podría interpretarse como computadora, en lugar de 'primera caja de Andy'
+let pcAndy = "oso"; 
+
+// Nombres de variables poco claros y desordenados
+let b, abc; // Evitar usar letras sueltas como nombres de variables
+pd = "oso"; // Mantener un orden y claridad en la declaración de variables
+
+// Ejemplos de nombres de variables recomendados
+
+// Nombre descriptivo que indica que se trata del primer Trasteo de Andy
+let primerTrasteoDeAndy = "oso"; 
+
+// Abreviación comúnmente entendida para URL, lo que hace que el nombre sea claro
+let urlDelUsuario = "http://www.google.com"; 
+
+// 'id' es una abreviatura reconocida para identificador, lo que facilita la comprensión
+let idDelUsuario = "123456789"; 
+
+// Declaración y uso de una variable con 'let'
+let contador; // Declaración de la variable contador
+contador = contador + 1; // Se incrementa el contador; 'let' permite modificar su valor
+
+// Declaración de una constante con 'const'
+// 'pi' es una constante que representa el valor matemático de pi, que no cambia
+const pi = 3.14; // 'const' se utiliza para valores fijos que no deben cambiar
