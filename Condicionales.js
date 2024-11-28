cfabe4ab5b0e2881af7c87c69e0f0f5c051bc6d8
Kevin <kevinquesada707@gmail.com>
2024-11-27 19:39:43 -0500

    Implementing conditional structures in JavaScript:

---
+++ Condicionales.js
@@ -0,0 +1,16 @@
+// Estructuras Condicionales o Estructuras de Control en JavaScript
+
+// Definimos una variable 'nombre' con el valor "Nico"
+let nombre = "Nico"
+
+// Iniciamos una estructura condicional 'if' para verificar el valor de 'nombre'
+if (nombre === "Diego") {
+    // Si 'nombre' es igual a "Diego", se ejecuta este bloque
+    console.log("Hola Diego")
+} else if (nombre === "Nico") {
+    // Si 'nombre' es igual a "Nico", se ejecuta este bloque
+    console.log("Hola Nico")
+} else {
+    // Si ninguna de las condiciones anteriores se cumple, se ejecuta este bloque
+    console.log("Nombre no Encontrado")
+}
